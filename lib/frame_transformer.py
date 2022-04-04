5bd7e4c55806d1e0a6b335e31a7af648afc1012a
carperbr <carperbr@gmail.com>
2022-04-03 23:47:57 -0400

    removing comment; change seems to be learning quite well, need to let it train and see how it does after training for a while

--- lib/frame_transformer.py
+++ lib/frame_transformer.py
@@ -16,10 +16,10 @@ class FrameTransformer(nn.Module):
 
     def forward(self, x):
         x = x[:, :, :self.max_bin]
-        out = torch.sigmoid(self.transformer(x))
+
         out = F.pad(
-            input=out,
-            pad=(0, 0, 0, self.output_bin - out.size()[2]),
+            input=torch.sigmoid(self.transformer(x)),
+            pad=(0, 0, 0, self.output_bin - self.max_bin),
             mode='replicate'
         )
 
@@ -56,7 +56,6 @@ class FrameTransformerNet(nn.Module):
         self.dec1_transformer = nn.ModuleList([FrameTransformerDecoder(nout * 2 + i, nout * 2 + num_encoders, num_bands, cropsize, n_fft, downsamples=1, feedforward_dim=feedforward_dim, bias=bias) for i in range(num_decoders)])
         self.dec1 = Decoder(nout * (1 + 2) + num_decoders + num_encoders, nout * 1, kernel_size=3, padding=1)
 
-        # doesn't seem like this is actually helpful, but currently testing. will likely remove this and enc1_transformer shortly.
         self.out_transformer = nn.ModuleList([FrameTransformerDecoder(nout + i, nout + num_encoders, num_bands, cropsize, n_fft, downsamples=0, feedforward_dim=feedforward_dim, bias=bias) for i in range(2)])
 
     def __call__(self, x):
@@ -309,10 +308,11 @@ class MultibandFrameAttention(nn.Module):
         self.v_proj = nn.Linear(bins, bins, bias=bias)
         self.o_proj = nn.Linear(bins, bins, bias=bias)
 
+        # relative positional encoding
         self.er = nn.Parameter(torch.empty(bins // num_bands, cropsize))
         nn.init.kaiming_uniform_(self.er, a=math.sqrt(5))
 
-        # I don't actually know if this helps (doesn't seem to hurt though); the general idea was to allow each band to focus on different regions in relation to its position to allow for some locality to be learned if useful.
+        # I don't actually know if this helps (doesn't seem to hurt though); the general idea was to allow each band to focus on different regions in relation to each frame's position to allow for some locality to be learned if useful.
         self.register_buffer('distances', torch.empty(num_bands, cropsize, cropsize))
         self.distance_weight = nn.Parameter(torch.empty(num_bands).unsqueeze(1).expand((-1, cropsize)).unsqueeze(2).clone())
         nn.init.kaiming_uniform_(self.distance_weight, a=math.sqrt(5.0))
