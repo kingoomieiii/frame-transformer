9f8e36237f20cba44eef8c0fa781585ba27e00bc
carperbr <carperbr@gmail.com>
2022-04-16 22:30:05 -0400

    more updates

--- lib/warmup_lr.py
+++ lib/warmup_lr.py
@@ -4,6 +4,7 @@ class WarmupLR(object):
     def __init__(self, optimizer, target_lr=1e-3, num_steps=16000, current_step=0, verbose=False, verbose_skip_steps=1000):
         self.target_lr = target_lr
         self.num_steps = num_steps
+        self.starting_step = current_step
         self.current_step = current_step
         self.verbose = verbose
         self.verbose_skip_steps = verbose_skip_steps
@@ -16,7 +17,7 @@ class WarmupLR(object):
         self._reset()
 
     def _reset(self):
-        self.current_step = 1
+        self.current_step = self.starting_step
         self.current_lr = (self.current_step) * (self.target_lr / self.num_steps)
         for i, param_group in enumerate(self.optimizer.param_groups):
             param_group['lr'] = self.current_lr
