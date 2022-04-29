0cff27d60fbeb3d2be5f878cbcdd6061b29ed0f5
carperbr <carperbr@gmail.com>
2022-04-28 21:56:20 -0400

    updating warmup to be hit actual target and not start at 0

--- lib/warmup_lr.py
+++ lib/warmup_lr.py
@@ -1,7 +1,7 @@
 from torch.optim import Optimizer
 
 class WarmupLR(object):
-    def __init__(self, optimizer, target_lr=1e-3, num_steps=16000, current_step=0, verbose=False, verbose_skip_steps=1000):
+    def __init__(self, optimizer, target_lr=1e-3, num_steps=16000, current_step=0, verbose=False, verbose_skip_steps=1):
         self.target_lr = target_lr
         self.num_steps = num_steps
         self.starting_step = current_step
@@ -17,7 +17,7 @@ class WarmupLR(object):
         self._reset()
 
     def _reset(self):
-        self.current_lr = (self.current_step) * (self.target_lr / self.num_steps)
+        self.current_lr = (self.current_step+1) * (self.target_lr / (self.num_steps+1))
         for i, param_group in enumerate(self.optimizer.param_groups):
             param_group['lr'] = self.current_lr
             if self.verbose and self.current_step % self.verbose_skip_steps == 0:
@@ -26,11 +26,11 @@ class WarmupLR(object):
         self.current_step = self.current_step + 1
 
     def step(self):
-        if self.current_step < self.num_steps:
-            self.current_lr = (self.current_step+1) * (self.target_lr / self.num_steps)
+        if self.current_step < self.num_steps+1:
+            self.current_lr = (self.current_step+1) * (self.target_lr / (self.num_steps+1))
             for i, param_group in enumerate(self.optimizer.param_groups):
                 param_group['lr'] = self.current_lr
-                if self.verbose and self.current_step % self.verbose_skip_steps == 0:
+                if self.verbose:
                     print(' Step {:5d} of {:5d}: increased learning rate'
                             ' of group {} to {:.4e}.'.format(self.current_step, self.num_steps, i, self.current_lr))
             self.current_step = self.current_step + 1
