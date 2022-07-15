b23f589524611b97b950941b557a43419938c1c8
carperbr <carperbr@gmail.com>
2022-07-14 20:30:33 -0400

    adding in skip attention

--- frame_primer/frame_resnet.py
+++ frame_primer/frame_resnet.py
@@ -54,9 +54,9 @@ class MultibandFrameAttention(nn.Module):
 
         return o, qk
 
-class FrameAttention(nn.Module):
-    def __init__(self, channels, skip_channels=None, num_bands=4, cropsize=1024, bias=False, downsamples=0, n_fft=2048):
-        super(FrameAttention, self).__init__()
+class SelfAttention(nn.Module):
+    def __init__(self, channels, num_bands=4, cropsize=1024, bias=False, downsamples=0, n_fft=2048):
+        super(SelfAttention, self).__init__()
 
         bins = n_fft // 2
         if downsamples > 0:
@@ -78,8 +78,32 @@ class FrameAttention(nn.Module):
 
         return h
 
+class SkipAttention(nn.Module):
+    def __init__(self, channels, skip_channels, num_bands=8, cropsize=1024, bias=False, downsamples=0, n_fft=2048):
+        super(SkipAttention, self).__init__()
+
+        bins = n_fft // 2
+        if downsamples > 0:
+            for _ in range(downsamples):
+                bins = ((bins - 1) // 2) + 1
+
+        self.bins = bins
+        self.cropsize = cropsize
+        self.num_bands = num_bands
+
+        self.in_project = nn.Conv2d(channels, 1, kernel_size=(3,1), padding=(1,0), bias=bias)
+        self.skip_project = nn.Conv2d(skip_channels, 1, kernel_size=(3,1), padding=(1,0), bias=bias)
+        self.attn = MultibandFrameAttention(num_bands=num_bands, bins=bins, cropsize=cropsize)
+
+    def __call__(self, x, skip):
+        h = self.in_project(x).transpose(1,3).squeeze(-1)
+        mem = self.skip_project(skip).transpose(1,3).squeeze(-1)
+        h, _ = self.attn(h, mem)
+        
+        return h.transpose(1,2).unsqueeze(1)
+
 class ResBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, activ=nn.LeakyReLU, cropsize=1024, downsamples=0, n_fft=2048, attention=False, num_bands=8):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, activ=nn.LeakyReLU, cropsize=1024, downsamples=0, n_fft=2048, attention=False, num_bands=8, skip_channels=None):
         super(ResBlock, self).__init__()
 
         bins1 = n_fft // 2
@@ -92,21 +116,28 @@ class ResBlock(nn.Module):
             for _ in range(downsamples):
                 bins2 = ((bins2 - 1) // 2) + 1
 
-        self.attention = FrameAttention(in_channels, num_bands=num_bands, cropsize=cropsize, downsamples=downsamples-1 if stride > 1 else downsamples, n_fft=n_fft) if attention else None
+        self.self_attention = SelfAttention(in_channels, num_bands=num_bands, cropsize=cropsize, downsamples=downsamples-1 if stride > 1 else downsamples, n_fft=n_fft) if attention else None
         self.norm1 = nn.LayerNorm(bins1)
+
+        self.skip_attention = SkipAttention(in_channels, skip_channels, num_bands=num_bands, cropsize=cropsize, bias=False, downsamples=downsamples-1 if stride > 1 else downsamples, n_fft=n_fft) if skip_channels is not None else None
+        self.norm2 = nn.LayerNorm(bins1) if skip_channels is not None else None
 
         self.identity = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0, stride=(stride, 1), bias=False) if in_channels != out_channels or stride > 1 else nn.Identity()
         self.conv1 = nn.Conv2d(in_channels, out_channels * 2, kernel_size=(kernel_size, 1), padding=(padding,0), stride=1, bias=False)
         self.relu = nn.LeakyReLU(inplace=True)
         self.conv2 = nn.Conv2d(out_channels * 2, out_channels, kernel_size=(kernel_size,1), padding=(padding,0), stride=(stride, 1), bias=False)
-        self.norm2 = nn.LayerNorm(bins2)
+        self.norm3 = nn.LayerNorm(bins2)
 
-    def __call__(self, x):
-        h = self.attention(x)
+    def __call__(self, x, skip=None):
+        h = self.self_attention(x)
         x = self.norm1((x + h).transpose(2,3)).transpose(2,3)
 
+        if skip is not None:
+            h = self.skip_attention(x, skip)
+            x = self.norm2((x + h).transpose(2,3)).transpose(2,3)
+
         h = self.conv2(self.relu(self.conv1(x)))
-        x = self.norm2((self.identity(x) + h).transpose(2,3)).transpose(2,3)
+        x = self.norm3((self.identity(x) + h).transpose(2,3)).transpose(2,3)
 
         return h
         
@@ -125,13 +156,13 @@ class FrameResEncoder(nn.Module):
         return x
 
 class FrameResDecoder(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1, upsample=True, activ=nn.LeakyReLU, norm=True, dropout=False, cropsize=1024, downsamples=0, n_fft=2048, num_res_blocks=1, num_bands=8):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1, upsample=True, activ=nn.LeakyReLU, norm=True, dropout=False, cropsize=1024, downsamples=0, n_fft=2048, num_res_blocks=1, num_bands=8, skip_channels=None):
         super(FrameResDecoder, self).__init__()
 
         self.cropsize = cropsize
         self.dowsnsamples = downsamples
         self.upsample = nn.Upsample(scale_factor=(2,1), mode='bilinear', align_corners=True) if upsample else nn.Identity()
-        self.body = nn.ModuleList([ResBlock(in_channels if i == 0 else out_channels, out_channels, kernel_size=kernel_size, padding=padding, cropsize=cropsize, downsamples=downsamples, n_fft=n_fft, attention=True, num_bands=num_bands) for i in range(num_res_blocks)])
+        self.body = nn.ModuleList([ResBlock(in_channels if i == 0 else out_channels, out_channels, kernel_size=kernel_size, padding=padding, cropsize=cropsize, downsamples=downsamples, n_fft=n_fft, attention=True, num_bands=num_bands, skip_channels=skip_channels) for i in range(num_res_blocks)])
 
     def __call__(self, x, skip=None):
         x = self.upsample(x)
@@ -140,27 +171,27 @@ class FrameResDecoder(nn.Module):
             x = torch.cat((x, skip), dim=1)
 
         for module in self.body:
-            x = module(x)
+            x = module(x, skip)
 
         return x
 
 class FrameResUNet(nn.Module):
-    def __init__(self, nin, nout, cropsize=2048, n_fft=2048, num_res_blocks=2, num_bands=8):
+    def __init__(self, nin, nout, cropsize=2048, n_fft=2048, num_res_encoders=2, num_res_decoders=2, num_bands=8):
         super(FrameResUNet, self).__init__()
 
         self.max_bin = n_fft // 2
         self.output_bin = n_fft // 2 + 1
         self.cropsize = cropsize
 
-        self.enc1 = FrameResEncoder(nin, out_channels=nout, kernel_size=3, stride=1, padding=1, downsamples=0, cropsize=cropsize, n_fft=n_fft, num_res_blocks=num_res_blocks)
-        self.enc2 = FrameResEncoder(nout, out_channels=nout * 2, kernel_size=3, stride=2, padding=1, downsamples=0, cropsize=cropsize, n_fft=n_fft, num_res_blocks=num_res_blocks)
-        self.enc3 = FrameResEncoder(nout * 2, out_channels=nout * 4, kernel_size=3, stride=2, padding=1, downsamples=1, cropsize=cropsize, n_fft=n_fft, num_res_blocks=num_res_blocks)
-        self.enc4 = FrameResEncoder(nout * 4, out_channels=nout * 6, kernel_size=3, stride=2, padding=1, downsamples=2, cropsize=cropsize, n_fft=n_fft, num_res_blocks=num_res_blocks)
-        self.enc5 = FrameResEncoder(nout * 6, out_channels=nout * 8, kernel_size=3, stride=2, padding=1, downsamples=3, cropsize=cropsize, n_fft=n_fft, num_res_blocks=num_res_blocks)
-        self.dec4 = FrameResDecoder(nout * (6 + 8), nout * 6, kernel_size=3, padding=1, cropsize=cropsize, num_res_blocks=num_res_blocks, downsamples=3)
-        self.dec3 = FrameResDecoder(nout * (4 + 6), nout * 4, kernel_size=3, padding=1, cropsize=cropsize, num_res_blocks=num_res_blocks, downsamples=2)
-        self.dec2 = FrameResDecoder(nout * (2 + 4), nout * 2, kernel_size=3, padding=1, cropsize=cropsize, num_res_blocks=num_res_blocks, downsamples=1)
-        self.dec1 = FrameResDecoder(nout * (1 + 2), nout, kernel_size=3, padding=1, cropsize=cropsize, num_res_blocks=num_res_blocks, downsamples=0)
+        self.enc1 = FrameResEncoder(nin, out_channels=nout, kernel_size=3, stride=1, padding=1, downsamples=0, cropsize=cropsize, n_fft=n_fft, num_res_blocks=num_res_encoders, num_bands=num_bands)
+        self.enc2 = FrameResEncoder(nout, out_channels=nout * 2, kernel_size=3, stride=2, padding=1, downsamples=0, cropsize=cropsize, n_fft=n_fft, num_res_blocks=num_res_encoders, num_bands=num_bands)
+        self.enc3 = FrameResEncoder(nout * 2, out_channels=nout * 4, kernel_size=3, stride=2, padding=1, downsamples=1, cropsize=cropsize, n_fft=n_fft, num_res_blocks=num_res_encoders, num_bands=num_bands)
+        self.enc4 = FrameResEncoder(nout * 4, out_channels=nout * 6, kernel_size=3, stride=2, padding=1, downsamples=2, cropsize=cropsize, n_fft=n_fft, num_res_blocks=num_res_encoders, num_bands=num_bands)
+        self.enc5 = FrameResEncoder(nout * 6, out_channels=nout * 8, kernel_size=3, stride=2, padding=1, downsamples=3, cropsize=cropsize, n_fft=n_fft, num_res_blocks=num_res_encoders, num_bands=num_bands)
+        self.dec4 = FrameResDecoder(nout * (6 + 8), nout * 6, kernel_size=3, padding=1, cropsize=cropsize, num_res_blocks=num_res_decoders, downsamples=3, num_bands=num_bands, skip_channels=nout * 6)
+        self.dec3 = FrameResDecoder(nout * (4 + 6), nout * 4, kernel_size=3, padding=1, cropsize=cropsize, num_res_blocks=num_res_decoders, downsamples=2, num_bands=num_bands, skip_channels=nout * 4)
+        self.dec2 = FrameResDecoder(nout * (2 + 4), nout * 2, kernel_size=3, padding=1, cropsize=cropsize, num_res_blocks=num_res_decoders, downsamples=1, num_bands=num_bands, skip_channels=nout * 2)
+        self.dec1 = FrameResDecoder(nout * (1 + 2), nout, kernel_size=3, padding=1, cropsize=cropsize, num_res_blocks=num_res_decoders, downsamples=0, num_bands=num_bands, skip_channels=nout)
         self.out = nn.Conv2d(nout, 2, kernel_size=1, padding=0, bias=False)
 
     def __call__(self, x):
