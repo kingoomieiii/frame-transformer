b486f6fcff9babb180122d3614a5109d3dd0ffcd
carperbr <carperbr@gmail.com>
2022-08-30 22:41:32 -0400

    trying out a weird learned scaling

--- frame_transformer.py
+++ frame_transformer.py
@@ -159,8 +159,8 @@ class MultichannelMultiheadAttention(nn.Module):
         self.num_heads = num_heads
         self.rotary_embedding = RotaryEmbedding(dim = features // num_heads)
 
-        self.g = nn.Parameter(torch.empty(channels, num_heads, 1, 1))
-        nn.init.ones_(self.g)
+        self.g_proj_a = MultichannelLinear(channels, channels, features, 1, depthwise=False)
+        self.g_proj_b = MultichannelLinear(channels, channels, 2, 1, depthwise=False)
         
         self.q_proj = nn.Sequential(
             MultichannelLinear(channels, channels, features, features, depthwise=False),
@@ -179,13 +179,22 @@ class MultichannelMultiheadAttention(nn.Module):
     def __call__(self, x, mem=None):
         b,c,h,w = x.shape
 
+        g = self.g_proj_a(x).squeeze(2)
+        g = self.g_proj_b(
+            torch.cat((
+                torch.mean(g, dim=2, keepdim=True),
+                torch.var(g, dim=2, keepdim=True)), dim=2
+            ).unsqueeze(-1)
+        ).unsqueeze(-1)
+
+        g = torch.mean(self.g_proj_a(x).squeeze(2), dim=2).unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)
         q = self.rotary_embedding.rotate_queries_or_keys(self.q_proj(x).transpose(2,3).reshape(b,c,w,self.num_heads,-1).permute(0,1,3,2,4))
         k = self.rotary_embedding.rotate_queries_or_keys(self.k_proj(x if mem is None else mem).transpose(2,3).reshape(b,c,w,self.num_heads,-1).permute(0,1,3,2,4)).transpose(3,4)
         v = self.v_proj(x if mem is None else mem).transpose(2,3).reshape(b,c,w,self.num_heads,-1).permute(0,1,3,2,4)
 
         with torch.cuda.amp.autocast_mode.autocast(enabled=False):
             qk = torch.matmul(q.float(), k.float()) / math.sqrt(h)
-            a = torch.matmul(F.softmax(self.g * qk, dim=-1),v.float()).transpose(2,3).reshape(b,c,w,-1).transpose(2,3)
+            a = torch.matmul(F.softmax(g.float() * qk, dim=-1),v.float()).transpose(2,3).reshape(b,c,w,-1).transpose(2,3)
 
         x = self.out_proj(a)
 
