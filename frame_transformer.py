a0fcbf0887b734fcb97b0b9d87bb7095b69424e6
carperbr <carperbr@gmail.com>
2022-08-31 00:31:04 -0400

    some cleaning up

--- frame_transformer.py
+++ frame_transformer.py
@@ -152,55 +152,54 @@ class FrameDecoder(nn.Module):
         return x
 
 class MultichannelMultiheadAttention(nn.Module):
-    def __init__(self, channels, num_heads, features, mixed_precision=False, focus_expansion=64):
+    def __init__(self, channels, num_heads, features, mixed_precision=False, focus_expansion=128):
         super().__init__()
 
         self.mixed_precision = mixed_precision
         self.num_heads = num_heads
-        self.rotary_embedding = RotaryEmbedding(dim = features // num_heads)
+        self.rotary_embedding = RotaryEmbedding(dim = features // num_heads, learned_freq=True)
 
-        self.ga_proj = MultichannelLinear(channels, channels, features, num_heads, depthwise=False)
+        self.f_proj1 = MultichannelLinear(channels, channels, features, num_heads, depthwise=False)
         
-        self.g_proj = nn.Sequential(
+        self.f_proj2 = nn.Sequential(
             MultichannelLinear(channels * num_heads, channels * num_heads, 6, focus_expansion, depthwise=False),
             SquaredReLU(),
             MultichannelLinear(channels * num_heads, channels * num_heads, focus_expansion, 1, depthwise=False))
         
         self.q_proj = nn.Sequential(
             MultichannelLinear(channels, channels, features, features, depthwise=False),
-            nn.Conv2d(channels, channels, kernel_size=(1,3), padding=(0,1), bias=False, groups=channels))
+            nn.Conv2d(channels, channels, kernel_size=(1,9), padding=(0,4), bias=False, groups=channels))
 
         self.k_proj = nn.Sequential(
             MultichannelLinear(channels, channels, features, features, depthwise=False),
-            nn.Conv2d(channels, channels, kernel_size=(1,3), padding=(0,1), bias=False, groups=channels))
+            nn.Conv2d(channels, channels, kernel_size=(1,9), padding=(0,4), bias=False, groups=channels))
 
         self.v_proj = nn.Sequential(
             MultichannelLinear(channels, channels, features, features, depthwise=False),
-            nn.Conv2d(channels, channels, kernel_size=(1,3), padding=(0,1), bias=False, groups=channels))
+            nn.Conv2d(channels, channels, kernel_size=(1,9), padding=(0,4), bias=False, groups=channels))
 
         self.out_proj = MultichannelLinear(channels, channels, features, features, depthwise=False)
 
     def __call__(self, x, mem=None):
         b,c,h,w = x.shape
 
-        g_a = self.ga_proj(x)
-        gc = torch.cat((
-            torch.median(g_a, dim=3, keepdim=True).values,
-            torch.mean(g_a, dim=3, keepdim=True), 
-            torch.var(g_a, dim=3, keepdim=True), 
-            torch.min(g_a, dim=3, keepdim=True).values, 
-            torch.max(g_a, dim=3, keepdim=True).values,
-            torch.linalg.norm(g_a, dim=3, keepdim=True),
-        ), dim=3)
-        
-        g = self.g_proj(gc.reshape(b,c*self.num_heads,1,6).transpose(2,3)).transpose(2,3).reshape(b,c,self.num_heads,1).unsqueeze(-1)
         q = self.rotary_embedding.rotate_queries_or_keys(self.q_proj(x).transpose(2,3).reshape(b,c,w,self.num_heads,-1).permute(0,1,3,2,4))
         k = self.rotary_embedding.rotate_queries_or_keys(self.k_proj(x if mem is None else mem).transpose(2,3).reshape(b,c,w,self.num_heads,-1).permute(0,1,3,2,4)).transpose(3,4)
         v = self.v_proj(x if mem is None else mem).transpose(2,3).reshape(b,c,w,self.num_heads,-1).permute(0,1,3,2,4)
 
+        f = self.f_proj1(x if mem is None else mem)        
+        f = torch.cat((
+            torch.median(f, dim=3, keepdim=True).values,
+            torch.mean(f, dim=3, keepdim=True), 
+            torch.var(f, dim=3, keepdim=True), 
+            torch.min(f, dim=3, keepdim=True).values, 
+            torch.max(f, dim=3, keepdim=True).values,
+            torch.linalg.norm(f, dim=3, keepdim=True)), dim=3)
+        f = self.f_proj2(f.reshape(b,c*self.num_heads,1,6).transpose(2,3)).transpose(2,3).reshape(b,c,self.num_heads,1).unsqueeze(-1)
+
         with torch.cuda.amp.autocast_mode.autocast(enabled=False):
             qk = torch.matmul(q.float(), k.float()) / math.sqrt(h)
-            a = torch.matmul(F.softmax(g.float() * qk, dim=-1),v.float()).transpose(2,3).reshape(b,c,w,-1).transpose(2,3)
+            a = torch.matmul(F.softmax(f.float() * qk, dim=-1),v.float()).transpose(2,3).reshape(b,c,w,-1).transpose(2,3)
 
         x = self.out_proj(a)
 
