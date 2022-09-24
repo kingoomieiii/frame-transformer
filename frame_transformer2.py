82e1d305147f4533c856c75e5e2a08aab3c8f080
carperbr <carperbr@gmail.com>
2022-09-24 04:28:16 -0400

    making some more changes which have allowd to cross the 1 billion parameter mark

--- frame_transformer2.py
+++ frame_transformer2.py
@@ -5,20 +5,20 @@ import torch.nn.functional as F
 from rotary_embedding_torch import RotaryEmbedding
 
 class FrameTransformer(nn.Module):
-    def __init__(self, in_channels=2, channels=2, dropout=0.1, n_fft=2048, num_heads=4, expansion=2, num_layers=12, pretraining=False, num_res_blocks=2):
+    def __init__(self, in_channels=2, channels=2, dropout=0.1, n_fft=2048, num_heads=4, expansion=2, num_layers=13, pretraining=False, num_res_blocks=2):
         super(FrameTransformer, self).__init__()
         
         self.max_bin = n_fft // 2
         self.output_bin = n_fft // 2 + 1
-
-        self.dropout = nn.Dropout2d(dropout) if not pretraining else None
 
         self.enc1 = FrameEncoder(in_channels, channels, self.max_bin, downsample=False, num_blocks=num_res_blocks)
         self.enc2 = FrameEncoder(channels, channels * 2, self.max_bin, num_blocks=num_res_blocks)
         self.enc3 = FrameEncoder(channels * 2, channels * 4, self.max_bin // 2, num_blocks=num_res_blocks)
         self.enc4 = FrameEncoder(channels * 4, channels * 8, self.max_bin // 4, num_blocks=num_res_blocks)
         self.enc5 = FrameEncoder(channels * 8, channels * 16, self.max_bin // 8, num_blocks=num_res_blocks)
-        self.encoder = nn.Sequential(*[MultichannelTransformerEncoder(channels * 16, self.max_bin // 16, num_heads=num_heads, dropout=dropout, expansion=expansion) for _ in range(num_layers)]) if not pretraining else None
+        self.enc6 = FrameEncoder(channels * 16, channels * 32, self.max_bin // 16, num_blocks=num_res_blocks)
+        self.encoder = nn.Sequential(*[MultichannelTransformerEncoder(channels * 32, self.max_bin // 32, num_heads=num_heads, dropout=dropout, expansion=expansion) for _ in range(num_layers)]) if not pretraining else None
+        self.dec5 = FrameDecoder(channels * 32, channels * 16, self.max_bin // 16, num_blocks=num_res_blocks)
         self.dec4 = FrameDecoder(channels * 16, channels * 8, self.max_bin // 8, num_blocks=num_res_blocks)
         self.dec3 = FrameDecoder(channels * 8, channels * 4, self.max_bin // 4, num_blocks=num_res_blocks)
         self.dec2 = FrameDecoder(channels * 4, channels * 2, self.max_bin // 2, num_blocks=num_res_blocks)
@@ -44,11 +44,13 @@ class FrameTransformer(nn.Module):
         e3 = self.enc3(e2)
         e4 = self.enc4(e3)
         e5 = self.enc5(e4)
+        e6 = self.enc6(e5)
 
         if self.encoder is not None:
-            e5 = self.dropout(self.encoder(e5))
+            e6 = self.encoder(e6)
 
-        d4 = self.dec4(e5, e4)
+        d5 = self.dec5(e6, e5)
+        d4 = self.dec4(d5, e4)
         d3 = self.dec3(d4, e3)
         d2 = self.dec2(d3, e2)
         d1 = self.dec1(d2, e1)
@@ -104,29 +106,6 @@ class FrameDecoder(nn.Module):
 
         return x
 
-class MultichannelLinear(nn.Module):
-    def __init__(self, in_channels, out_channels, in_features, out_features, positionwise=True, depthwise=False):
-        super(MultichannelLinear, self).__init__()
-
-        self.weight_pw = None
-        if in_features != out_features or positionwise:
-            self.weight_pw = nn.Parameter(torch.empty(in_channels, out_features, in_features))
-            nn.init.uniform_(self.weight_pw, a=-1/math.sqrt(in_features), b=1/math.sqrt(in_features))
-
-        self.weight_dw = None
-        if in_channels != out_channels or depthwise:
-            self.weight_dw = nn.Parameter(torch.empty(out_channels, in_channels))
-            nn.init.uniform_(self.weight_dw, a=-1/math.sqrt(in_channels), b=1/math.sqrt(in_channels))
-
-    def __call__(self, x):
-        if self.weight_pw is not None:
-            x = torch.matmul(x.transpose(2,3), self.weight_pw.transpose(1,2)).transpose(2,3)
-
-        if self.weight_dw is not None:
-            x = torch.matmul(x.transpose(1,3), self.weight_dw.t()).transpose(1,3)
-        
-        return x
-
 class MultichannelAttention(nn.Module):
     def __init__(self, channels, num_heads, features, mixed_precision=False, dropout=0.1):
         super().__init__()
@@ -134,20 +113,10 @@ class MultichannelAttention(nn.Module):
         self.mixed_precision = mixed_precision
         self.num_heads = num_heads
         self.rotary_embedding = RotaryEmbedding(dim = features // num_heads)
-
-        self.q_proj = nn.Sequential(
-            nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False),
-            MultichannelLinear(channels, channels, features, features))
-
-        self.k_proj = nn.Sequential(
-            nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False),
-            MultichannelLinear(channels, channels, features, features))
-            
-        self.v_proj = nn.Sequential(
-            nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False),
-            MultichannelLinear(channels, channels, features, features))
-            
-        self.out_proj = MultichannelLinear(channels, channels, features, features)
+        self.q_proj = nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False)
+        self.k_proj = nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False)            
+        self.v_proj = nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False)            
+        self.out_proj = nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False)
 
     def __call__(self, x, mem=None):
         b,c,h,w = x.shape
@@ -169,6 +138,7 @@ class MultichannelTransformerEncoder(nn.Module):
         super(MultichannelTransformerEncoder, self).__init__()
 
         self.activate = SquaredReLU()
+        self.dropout = nn.Dropout2d(dropout)
 
         self.norm1 = nn.LayerNorm(features)
         self.attn = MultichannelAttention(channels, num_heads, features)
@@ -180,10 +150,10 @@ class MultichannelTransformerEncoder(nn.Module):
     def __call__(self, x):
         z = self.norm1(x.transpose(2,3)).transpose(2,3)
         z = self.attn(z)
-        x = x + z
+        x = x + self.dropout(z)
 
         z = self.norm2(x.transpose(2,3)).transpose(2,3)
         z = self.conv2(self.activate(self.conv1(z)))
-        x = x + z
+        x = x + self.dropout(z)
 
         return x
