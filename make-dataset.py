39323268c6441a3731c0ba78073493ba4fefee39
carperbr <carperbr@gmail.com>
2022-12-04 22:16:49 -0500

    checking in horrifically messy dataset generator script to regenerate on new machine

--- make-dataset.py
+++ make-dataset.py
@@ -1,10 +1,10 @@
 from lib.dataset import make_validation_set, train_val_split, make_vocal_stems, make_dataset, make_mix_dataset
 
-vocal_dataset = "G://vocals"
-dataset = "G://new-inst"
-validation = "E://Projects/vocal-remover-data/validation"
+vocal_dataset = "J://vocals"
+dataset = "G://inst-staging"
+validation = "G://dataset/validation"
 
-cropsize = 2048
+cropsize = 256
 hop_length = 1024
 fft = 2048
 
@@ -15,7 +15,18 @@ dirs = [
     #("G://new", 'G://', False),
     #("C://new-inst", 'G://', True),
     #("G://training_p3", 'C://', True),
-    ("G://full-albums", 'G://', True),
+    ("J://dataset/training_p12", 'C://', True, False),
+    # ("J://dataset/inst-03", 'D://', True, False),
+    # ("J://dataset/training_p1", 'D://', True, False),
+    # ("J://dataset/training_p11", 'D://', True, False),
+    # ("J://dataset/training_p12", 'D://', True, False),
+    # ("K://dataset/training_p2", 'F://', True, False),
+    # ("K://dataset/training_p3", 'F://', True, False),
+    # ("K://dataset/training_p4", 'F://', True, False),
+    # ("J://dataset/training_p12", 'C://', False, False),
+    # ("K://dataset/training_p6", 'C://', False, False),
+    # ("K://dataset/training_p7", 'C://', False, False),
+    # ("J://dataset/training_p13", 'H://', True, False),
     #("G://training_p5", 'G://', False),
     #("E://Projects/vocal-remover-data/training_p3", 'C://', True),
     #("E://Projects/vocal-remover-data/validation", 'C://', False),
@@ -33,14 +44,25 @@ for dir in dirs:
     if not dir[2]:
         print(train_filelist)
 
-    val_dataset = make_mix_dataset(
-        filelist=train_filelist,
-        cropsize=cropsize,
-        sr=44100,
-        hop_length=hop_length,
-        n_fft=fft,
-        root=dir[1],
-        is_validation=True)
+    if not dir[3]:
+        val_dataset = make_dataset(
+            filelist=train_filelist,
+            cropsize=cropsize,
+            sr=44100,
+            hop_length=hop_length,
+            n_fft=fft,
+            root=dir[1],
+            is_validation=True,
+            suffix='_PAIRS' if not dir[2] else '')
+    else:
+        val_dataset = make_mix_dataset(
+            filelist=train_filelist,
+            cropsize=cropsize,
+            sr=44100,
+            hop_length=hop_length,
+            n_fft=fft,
+            root=dir[1],
+            is_validation=True)
 
 # val_filelist, _ = train_val_split(
 #     dataset_dir=validation,
@@ -51,11 +73,12 @@ for dir in dirs:
 
 # val_dataset = make_dataset(
 #     filelist=val_filelist,
-#     cropsize=cropsize//2,
+#     cropsize=cropsize,
 #     sr=44100,
 #     hop_length=hop_length,
 #     n_fft=fft,
-#     root="D://",
-#     is_validation=True)
+#     root="C://",
+#     is_validation=True,
+#     suffix='_VALIDATION')
 
-#make_vocal_stems(dataset=vocal_dataset, root="G://", cropsize=cropsize, hop_length=hop_length, n_fft=fft)
+# make_vocal_stems(dataset=vocal_dataset, root="D://", cropsize=cropsize, hop_length=hop_length, n_fft=fft)
