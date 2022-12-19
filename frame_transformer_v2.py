d2db91e058c703a8d3189440c2cb804f49cfd7f4
carperbr <carperbr@gmail.com>
2022-12-18 23:57:32 -0500

    cleaning up current experiment

--- frame_transformer_v2.py
+++ frame_transformer_v2.py
@@ -16,15 +16,6 @@ class FrameTransformer(nn.Module):
 
         self.enc1 = FrameEncoder(in_channels, channels, self.max_bin, downsample=False)
         self.transformer_encoder = nn.ModuleList([FrameTransformerEncoder(channels, self.max_bin, dropout=dropout, expansion=expansion, num_heads=num_heads) for _ in range(num_layers)])
-        # self.enc2 = FrameEncoder(channels * 1, channels * 2, self.max_bin)
-        # self.enc3 = FrameEncoder(channels * 2, channels * 4, self.max_bin // 2)
-        # self.enc4 = FrameEncoder(channels * 4, channels * 6, self.max_bin // 4)
-        # self.enc5 = FrameEncoder(channels * 6, channels * 8, self.max_bin // 8)
-        # self.dec4 = FrameDecoder(channels * 8, channels * 6, self.max_bin // 8)
-        # self.dec3 = FrameDecoder(channels * 6, channels * 4, self.max_bin // 4)
-        # self.dec2 = FrameDecoder(channels * 4, channels * 2, self.max_bin // 2)
-        # self.dec1 = FrameDecoder(channels * 2, channels * 1, self.max_bin)
-        #self.transformer_decoder = nn.ModuleList([FrameTransformerDecoder(channels, self.max_bin, dropout=dropout, expansion=expansion, num_heads=num_heads) for _ in range(num_layers)])
         self.out = nn.Parameter(torch.empty(out_channels, channels))
         nn.init.uniform_(self.out, a=-1/math.sqrt(channels), b=1/math.sqrt(channels))
 
@@ -33,18 +24,6 @@ class FrameTransformer(nn.Module):
 
         for encoder in self.transformer_encoder:
             e1 = encoder(e1)
-
-        # e2 = self.enc2(e1)
-        # e3 = self.enc3(e2)
-        # e4 = self.enc4(e3)
-        # e5 = self.enc5(e4)
-        # d4 = self.dec4(e5, e4)
-        # d3 = self.dec3(d4, e3)
-        # d2 = self.dec2(d3, e2)
-        # d1 = self.dec1(d2, e1)
-
-        # for decoder in self.transformer_decoder:
-        #     d1 = decoder(e1, skip=e1)
 
         out = torch.matmul(e1.transpose(1,3), self.out.t()).transpose(1,3)
 
@@ -77,19 +56,6 @@ class FrameEncoder(nn.Module):
         self.body = nn.Sequential(*[ResBlock(in_channels if i == 0 else out_channels, out_channels, features, downsample=True if i == num_blocks - 1 and downsample else False) for i in range(num_blocks)])
 
     def __call__(self, x):
-        x = self.body(x)
-
-        return x
-
-class FrameDecoder(nn.Module):
-    def __init__(self, in_channels, out_channels, features, num_blocks=3):
-        super(FrameDecoder, self).__init__()
-
-        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        self.body = nn.Sequential(*[ResBlock(in_channels if i == 0 else out_channels, out_channels, features) for i in range(num_blocks)])
-
-    def __call__(self, x, skip):
-        x = self.upsample(x)
         x = self.body(x)
 
         return x
@@ -149,35 +115,6 @@ class FrameTransformerEncoder(nn.Module):
         h = x + self.dropout(z)
 
         z = self.conv2(self.activate(self.conv1(self.norm2(h))))
-        h = h + self.dropout(z)
-
-        return h
-        
-class FrameTransformerDecoder(nn.Module):
-    def __init__(self, channels, features, dropout=0.1, expansion=4, num_heads=8):
-        super(FrameTransformerDecoder, self).__init__()
-
-        self.activate = SquaredReLU()
-        self.dropout = nn.Dropout(dropout)
-
-        self.norm1 = MultichannelLayerNorm(channels, features)
-        self.attn = MultichannelMultiheadAttention(channels, num_heads, features)
-
-        self.norm2 = MultichannelLayerNorm(channels, features)
-        self.attn2 = MultichannelMultiheadAttention(channels, num_heads, features)
-
-        self.norm3 = MultichannelLayerNorm(channels, features)
-        self.conv1 = MultichannelLinear(channels, channels, features, features * expansion, depthwise=True)
-        self.conv2 = MultichannelLinear(channels, channels, features * expansion, features, depthwise=True)
-        
-    def __call__(self, x, skip):        
-        z = self.attn(self.norm1(x))
-        h = x + self.dropout(z)
-
-        z = self.attn2(self.norm2(h), mem=skip)
-        h = h + self.dropout(z)
-
-        z = self.conv2(self.activate(self.conv1(self.norm3(h))))
         h = h + self.dropout(z)
 
         return h
