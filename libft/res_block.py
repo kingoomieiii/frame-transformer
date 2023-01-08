d35a2079104f3a59ea5d043fbf7033be261a44ba
carperbr <carperbr@gmail.com>
2023-01-08 08:27:07 -0500

    fixing reference to now removed squared_relu module

--- libft/res_block.py
+++ libft/res_block.py
@@ -1,14 +1,12 @@
 import torch
 import torch.nn as nn
 
-from libft.squared_relu import SquaredReLU
 from libft.multichannel_layernorm import MultichannelLayerNorm
 
 class ResBlock(nn.Module):
     def __init__(self, in_channels, out_channels, features, downsample=False, expansion=1):
         super(ResBlock, self).__init__()
 
-        self.activate = SquaredReLU()
         self.norm = MultichannelLayerNorm(in_channels, features)
         self.conv1 = nn.Conv2d(in_channels, out_channels * expansion, kernel_size=3, padding=1, bias=False)
         self.conv2 = nn.Conv2d(out_channels * expansion, out_channels, kernel_size=3, padding=1, bias=False, stride=2 if downsample else 1)
