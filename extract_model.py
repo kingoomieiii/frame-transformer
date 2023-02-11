33abd1fe0dd498926f7cda093a90bdb4dba0515f
carperbr <carperbr@gmail.com>
2023-02-10 21:47:06 -0500

    checking in a weird parallel BERT experiment

--- extract_model.py
+++ extract_model.py
@@ -1,5 +1,5 @@
 from transformers import AutoModel, BertModel
-from frame_transformer_v3 import FrameTransformer
+from libft.fbert import FrameTransformer
 import torch
 
 import argparse
@@ -19,8 +19,8 @@ def load_from_huggingface(transformer, name='bert-large-uncased'):
             transformer[idx].attn.k_proj.bias_pw.data = expand_linear_bias(layer.attention.self.key.bias, channels)
             transformer[idx].attn.v_proj.weight_pw.data = expand_weight(layer.attention.self.value.weight, channels)
             transformer[idx].attn.v_proj.bias_pw.data = expand_linear_bias(layer.attention.self.value.bias, channels)
-            transformer[idx].attn.out_proj.weight_pw.data = expand_weight(layer.attention.output.dense.weight, channels)
-            transformer[idx].attn.out_proj.bias_pw.data = expand_linear_bias(layer.attention.output.dense.bias, channels)
+            transformer[idx].attn.o_proj.weight_pw.data = expand_weight(layer.attention.output.dense.weight, channels)
+            transformer[idx].attn.o_proj.bias_pw.data = expand_linear_bias(layer.attention.output.dense.bias, channels)
             transformer[idx].norm1.weight.data = expand_weight(layer.attention.output.LayerNorm.weight, channels)
             transformer[idx].norm1.bias.data = expand_normal_bias(layer.attention.output.LayerNorm.bias, channels)
             transformer[idx].conv1.weight_pw.data = expand_weight(layer.intermediate.dense.weight, channels)
@@ -44,12 +44,13 @@ def expand_normal_bias(x, channels):
 
 def main():
     p = argparse.ArgumentParser()
-    p.add_argument('--name', type=str, default='bert-large-cased')
+    p.add_argument('--name', type=str, default='bert-large-uncased')
     p.add_argument('-n_fft', type=int, default=2048)
     p.add_argument('--num_layers', type=int, default=24)
-    p.add_argument('--channels', type=int, default=2)
+    p.add_argument('--channels', type=int, default=4)
     p.add_argument('--expansion', type=int, default=4)
     p.add_argument('--num_heads', type=int, default=16)
+    p.add_argument('--embedding', type=int, default=1024)
     p.add_argument('--output', type=str, default='H://models')
     args = p.parse_args()
 
