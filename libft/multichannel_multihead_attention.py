cc369e952733cfc5d5532724aa021dc22fa468e9
carperbr <carperbr@gmail.com>
2023-02-15 14:07:33 -0500

    adding back for stability, more info in comments

--- libft/multichannel_multihead_attention.py
+++ libft/multichannel_multihead_attention.py
@@ -12,32 +12,35 @@ class MultichannelMultiheadAttention(nn.Module):
 
         self.num_heads = num_heads
         self.rotary_embedding = RotaryEmbedding(features  // num_heads)
-        
+
         self.q_proj = nn.Sequential(
-            MultichannelLinear(channels, channels, features, features, depthwise=depthwise),
-            nn.Conv2d(channels, channels, kernel_size=(1,3), padding=(0,1), groups=channels))
+            nn.Conv2d(channels, channels, kernel_size=3, padding=1, groups=channels),
+            MultichannelLinear(channels, channels, features, features, depthwise=depthwise))
         
         self.k_proj = nn.Sequential(
-            MultichannelLinear(channels, channels, features, features, depthwise=depthwise),
-            nn.Conv2d(channels, channels, kernel_size=(1,3), padding=(0,1), groups=channels))
+            nn.Conv2d(channels, channels, kernel_size=3, padding=1, groups=channels),
+            MultichannelLinear(channels, channels, features, features, depthwise=depthwise))
         
         self.v_proj = nn.Sequential(
-            MultichannelLinear(channels, channels, features, features, depthwise=depthwise),
-            nn.Conv2d(channels, channels, kernel_size=(1,3), padding=(0,1), groups=channels))
+            nn.Conv2d(channels, channels, kernel_size=3, padding=1, groups=channels),
+            MultichannelLinear(channels, channels, features, features, depthwise=depthwise))
         
         self.o_proj = MultichannelLinear(channels, channels, features, features, depthwise=depthwise)
 
     def __call__(self, x, mem=None, prev_qk=None):
         b,c,h,w = x.shape
-        q = self.q_proj(x).transpose(2,3).reshape(b,c,w,self.num_heads,-1).permute(0,1,3,2,4)
-        k = self.k_proj(x if mem is None else mem).transpose(2,3).reshape(b,c,w,self.num_heads,-1).permute(0,1,3,4,2)
+        q = self.rotary_embedding.rotate_queries_or_keys(self.q_proj(x).transpose(2,3).reshape(b,c,w,self.num_heads,-1).permute(0,1,3,2,4))
+        k = self.rotary_embedding.rotate_queries_or_keys(self.k_proj(x if mem is None else mem).transpose(2,3).reshape(b,c,w,self.num_heads,-1).permute(0,1,3,2,4)).transpose(3,4)
         v = self.v_proj(x if mem is None else mem).transpose(2,3).reshape(b,c,w,self.num_heads,-1).permute(0,1,3,2,4)
-        qk = torch.matmul(q.float(), k.float()) / math.sqrt(h)
 
-        if prev_qk is not None:
-            qk = qk + prev_qk
+        with torch.cuda.amp.autocast_mode.autocast(enabled=False):
+            qk = torch.matmul(q.float(), k.float()) / math.sqrt(h)
 
-        a = torch.matmul(F.softmax(qk, dim=-1),v.float()).transpose(2,3).reshape(b,c,w,-1).transpose(2,3)
+            if prev_qk is not None:
+                qk = qk + prev_qk
+
+            a = torch.matmul(F.softmax(qk, dim=-1),v.float()).transpose(2,3).reshape(b,c,w,-1).transpose(2,3)
+
         x = self.o_proj(a)
 
         return x, qk
