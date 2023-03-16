7c956c687808c8c877ba1f7a794ec6808b124483
carperbr <carperbr@gmail.com>
2023-03-15 22:20:27 -0400

    working on some new augmentations for vocals

---
+++ libft/dataset_utils.py
@@ -0,0 +1,109 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+from scipy.signal import stft, istft
+import librosa
+
+def apply_random_eq(X, min=0, max=2):
+    arr1 = F.interpolate(torch.rand((1, 1, 512,)) * (max - min) + min, size=(X.shape[1]), mode='linear', align_corners=True).squeeze(0).squeeze(0).numpy()
+    arr2 = F.interpolate(torch.rand((1, 1, 256,)) * (max - min) + min, size=(X.shape[1]), mode='linear', align_corners=True).squeeze(0).squeeze(0).numpy()
+    arr3 = F.interpolate(torch.rand((1, 1, 128,)) * (max - min) + min, size=(X.shape[1]), mode='linear', align_corners=True).squeeze(0).squeeze(0).numpy()
+    arr4 = F.interpolate(torch.rand((1, 1, 64,)) * (max - min) + min, size=(X.shape[1]), mode='linear', align_corners=True).squeeze(0).squeeze(0).numpy()
+    arr5 = F.interpolate(torch.rand((1, 1, 32,)) * (max - min) + min, size=(X.shape[1]), mode='linear', align_corners=True).squeeze(0).squeeze(0).numpy()
+    arr6 = F.interpolate(torch.rand((1, 1, 16,)) * (max - min) + min, size=(X.shape[1]), mode='linear', align_corners=True).squeeze(0).squeeze(0).numpy()
+    arr7 = F.interpolate(torch.rand((1, 1, 8,)) * (max - min) + min, size=(X.shape[1]), mode='linear', align_corners=True).squeeze(0).squeeze(0).numpy()
+    arr8 = F.interpolate(torch.rand((1, 1, 4,)) * (max - min) + min, size=(X.shape[1]), mode='linear', align_corners=True).squeeze(0).squeeze(0).numpy()
+    arr9 = F.interpolate(torch.rand((1, 1, 2,)) * (max - min) + min, size=(X.shape[1]), mode='linear', align_corners=True).squeeze(0).squeeze(0).numpy()
+    eq = (arr1 + arr2 + arr3 + arr4 + arr5 + arr6 + arr7 + arr8 + arr9) / 9.0
+    eq = np.clip(eq, 0, 1.5)
+    eq = np.expand_dims(eq, (0, 2))
+    return X * eq
+
+def apply_random_volume(X, min=0.5, max=1.5):
+    return X * np.random.uniform(min, max)
+
+def apply_harmonic_distortion(M, P, num_harmonics=2, gain=0.1, n_fft=2048, hop_length=1024):
+    left_M = M[0]
+    right_M = M[1]
+    left_P = P[0]
+    right_P = P[1]
+    
+    left_X = left_M * np.exp(1.j * left_P)
+    right_X = right_M * np.exp(1.j * right_P)
+
+    left_s = librosa.istft(left_X, hop_length=hop_length)
+    right_s = librosa.istft(right_X, hop_length=hop_length)
+
+    left_ds = np.copy(left_s)
+    right_ds = np.copy(right_s)
+    for h in range(2, num_harmonics + 1):
+        left_hs = np.roll(left_s, h, axis=-1)
+        right_hs = np.roll(right_s, h, axis=-1)
+        left_ds += (gain ** (h - 1)) * left_hs
+        right_ds += (gain ** (h - 1)) * right_hs
+
+    left_X = librosa.stft(left_ds, n_fft=n_fft, hop_length=hop_length)
+    right_X = librosa.stft(right_ds, n_fft=n_fft, hop_length=hop_length)
+
+    return np.array([np.abs(left_X), np.abs(right_X)])
+
+def apply_stereo_spatialization(X, c, alpha=1):
+    X = X / c
+    left, right = X[0], X[1]
+    avg = (left + right) * 0.5
+    left = alpha * left + (1 - alpha) * avg 
+    right = alpha * right + (1 - alpha) * avg
+    return np.clip(np.stack([left, right], axis=0), 0, 1) * c
+
+def apply_noise(X, gamma=0.95, sigma=0.4):
+    eps = np.random.normal(scale=sigma, size=X.shape)
+    return np.sqrt(gamma) * X + np.sqrt(1 - gamma) * eps
+
+def apply_dynamic_range_mod(X, threshold=0.5, ratio=4):
+    if np.random.uniform() < 0.5:
+        clipped = np.clip(X - threshold, 0, None)
+        compressed = clipped / ratio
+        return np.where(X > threshold, compressed + threshold, X)
+    else:
+        clipped = np.clip(X - threshold, None, 0)
+        expanded = clipped * ratio
+        return np.where(X < threshold, expanded + threshold, X)
+    
+def apply_channel_drop(X):
+    if np.random.uniform() < 0.5:
+        X[0] = 0
+    else:
+        X[1] = 0
+
+    return X
+    
+def apply_channel_swap(X):
+    return X[::-1]
+
+def apply_time_stretch(X, target_size):
+    if X.shape[2] > target_size:
+        size = np.random.randint(target_size // 16, X.shape[2])
+        start = np.random.randint(0, X.shape[0] - size)
+        cropped = X[:, :, start+size]
+        H = X[:, :, :target_size]
+        H.real = F.interpolate(torch.from_numpy(cropped.real).unsqueeze(0), size=(X.shape[1], target_size), mode='bilinear', align_corners=True).squeeze(0).numpy()
+        H.imag = F.interpolate(torch.from_numpy(cropped.imag).unsqueeze(0), size=(X.shape[1], target_size), mode='bilinear', align_corners=True).squeeze(0).numpy()
+        return H
+    else:
+        if np.random.uniform() < 0.5:
+            padded = np.pad(X, ((0, 0), (0, 0), (np.random.randint(0, target_size // 4), (target_size - X.shape[2]) + np.random.randint(0, target_size // 4))))
+            size = np.random.randint(target_size, padded.shape[2])
+            start = np.random.randint(0, padded.shape[2] - size)
+            cropped = padded[:, :, start:start+size]
+            H = padded[:, :, :target_size]
+            H.real = F.interpolate(torch.from_numpy(cropped.real).unsqueeze(0), size=(padded.shape[1], target_size), mode='bilinear', align_corners=True).squeeze(0).numpy()
+            H.imag = F.interpolate(torch.from_numpy(cropped.imag).unsqueeze(0), size=(padded.shape[1], target_size), mode='bilinear', align_corners=True).squeeze(0).numpy()
+            return H
+        else: 
+            size = np.random.randint(target_size // 8, target_size)
+            start = np.random.randint(0, X.shape[2] - size)
+            cropped = X[:, :, start:start+size]
+            H = X[:, :, :target_size]
+            H.real = F.interpolate(torch.from_numpy(cropped.real).unsqueeze(0), size=(X.shape[1], target_size), mode='bilinear', align_corners=True).squeeze(0).numpy()
+            H.imag = F.interpolate(torch.from_numpy(cropped.imag).unsqueeze(0), size=(X.shape[1], target_size), mode='bilinear', align_corners=True).squeeze(0).numpy()
+            return H
---
+++ libft/dataset_voxaug3.py
@@ -0,0 +1,149 @@
+import os
+import random
+import numpy as np
+import torch
+import torch.utils.data
+import torch.nn.functional as F
+import scipy.ndimage
+
+from libft.dataset_utils import apply_channel_drop, apply_channel_swap, apply_dynamic_range_mod, apply_harmonic_distortion, apply_noise, apply_random_eq, apply_stereo_spatialization, apply_time_stretch
+
+class VoxAugDataset(torch.utils.data.Dataset):
+    def __init__(self, path=[], vocal_path=[], is_validation=False, n_fft=2048, hop_length=1024, cropsize=256, seed=0, inst_rate=0.025, data_limit=None, predict_vocals=False, time_scaling=True):
+        self.is_validation = is_validation
+        self.vocal_list = []
+        self.curr_list = []
+        self.epoch = 0
+        self.inst_rate = inst_rate
+        self.predict_vocals = predict_vocals
+        self.time_scaling = time_scaling
+
+        self.n_fft = n_fft
+        self.hop_length = hop_length
+        self.cropsize = cropsize
+
+        for mp in path:
+            mixes = [os.path.join(mp, f) for f in os.listdir(mp) if os.path.isfile(os.path.join(mp, f))]
+
+            for m in mixes:
+                self.curr_list.append(m)
+            
+        if not is_validation and len(vocal_path) != 0:
+            for vp in vocal_path:
+                vox = [os.path.join(vp, f) for f in os.listdir(vp) if os.path.isfile(os.path.join(vp, f))]
+
+                for v in vox:
+                    self.vocal_list.append(v)
+
+            random.Random(seed).shuffle(self.vocal_list)
+
+        random.Random(seed+1).shuffle(self.curr_list)
+
+    def set_epoch(self, epoch):
+        self.epoch = epoch
+
+    def __len__(self):
+        return len(self.curr_list)
+
+    def _get_vocals(self, idx):
+        path = str(self.vocal_list[(self.epoch + idx) % len(self.vocal_list)])
+        vdata = np.load(path)
+        V, Vc = vdata['X'], vdata['c']
+
+        if np.random.uniform() < 0.5:
+            V = apply_time_stretch(V, self.cropsize)
+        elif V.shape[2] > self.cropsize:
+            start = np.random.randint(0, V.shape[2] - self.cropsize)
+            V = V[:, :, start:start+self.cropsize]
+
+        P = np.angle(V)
+        M = np.abs(V)
+
+        augmentations = [
+            (0.025, apply_channel_drop, {}),
+            (0.5, apply_channel_swap, {}),
+            (0.5, apply_dynamic_range_mod, { "threshold": np.random.uniform(0, 0.5), "ratio": np.random.randint(2,8) }),
+            (0.1, apply_harmonic_distortion, { "P": P, "num_harmonics": np.random.randint(1, 4), "gain": np.random.uniform(0.05, 0.5), "n_fft": self.n_fft, "hop_length": self.hop_length }),
+            (0.5, apply_noise, { "gamma": np.random.uniform(0.75, 1), "sigma": np.random.uniform(0.1,0.5) }),
+            (0.5, apply_random_eq, { "min": np.random.uniform(0,0.25), "max": np.random.uniform(1, 2) }),
+            (0.5, apply_stereo_spatialization, { "c": Vc, "alpha": np.random.uniform(0, 2) })
+        ]
+
+        random.shuffle(augmentations)
+
+        for p, aug, args in augmentations:
+            if np.random.uniform() < p:
+                M = aug(M, **args)
+
+        V = M * np.exp(1.j * P)
+
+        if np.random.uniform() < 0.5:
+            V = V[::-1]
+
+        return V
+    
+    def _augment_instruments(self, X, c):
+        if np.random.uniform() < 0.0125:
+            if np.random.uniform() < 0.5:
+                X[0] = 0
+            else:
+                X[1] = 0
+                
+        P = np.angle(X)
+        M = np.abs(X)
+        
+        if np.random.uniform() < 0.5:
+            M = apply_random_eq(M)
+
+        if np.random.uniform() < 0.5:
+            M = apply_noise(M)
+
+        if np.random.uniform() < 0.5:
+            if np.random.uniform() < 0.5:
+                M = apply_compression(M)
+            else:
+                M = apply_expansion(M)
+
+        if np.random.uniform() < 0.5:
+            M = M / c
+            alpha = np.random.uniform(0, 2)
+            M = np.clip(apply_stereo_spatialization(M, alpha), 0, 1) * c
+
+        X = M * np.exp(1.j * P)
+
+    def __getitem__(self, idx):
+        path = str(self.curr_list[idx % len(self.curr_list)])
+        data = np.load(path)
+        aug = 'Y' not in data.files
+
+        X, c = data['X'], data['c']
+        Y = X if aug else data['Y']
+        V = None
+        
+        if not self.is_validation:
+            if Y.shape[2] > self.cropsize:
+                start = np.random.randint(0, Y.shape[2] - self.cropsize + 1)
+                stop = start + self.cropsize
+                Y = Y[:,:,start:stop]
+
+            V = self._get_vocals(idx)
+            X = Y + V
+            c = np.max([c, np.abs(X).max()])
+
+            if np.random.uniform() < self.inst_rate:
+                X = Y
+
+            if np.random.uniform() < 0.5:
+                X = X[::-1]
+                Y = Y[::-1]
+        else:
+            if len(self.vocal_list) > 0:
+                vpath = self.vocal_list[idx % len(self.vocal_list)]
+                vdata = np.load(str(vpath))
+                V = vdata['X']
+                X = Y + V
+
+        X = np.clip(np.abs(X) / c, 0, 1)
+        Y = np.clip(np.abs(Y) / c, 0, 1)
+        
+        return X, Y
