71df95021753452160d907ce57a3aa2ad3f4af0b
carperbr <carperbr@gmail.com>
2023-03-24 12:30:37 -0400

    updates to augmentations

--- libft2/dataset_utils.py
+++ libft2/dataset_utils.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 import numpy as np
 import librosa
 
-def apply_random_eq(X, min=0, max=2):
+def apply_random_eq(X, P, min=0, max=2):
     arr1 = F.interpolate(torch.rand((1, 1, 512,)) * (max - min) + min, size=(X.shape[1]), mode='linear', align_corners=True).squeeze(0).squeeze(0).numpy()
     arr2 = F.interpolate(torch.rand((1, 1, 256,)) * (max - min) + min, size=(X.shape[1]), mode='linear', align_corners=True).squeeze(0).squeeze(0).numpy()
     arr3 = F.interpolate(torch.rand((1, 1, 128,)) * (max - min) + min, size=(X.shape[1]), mode='linear', align_corners=True).squeeze(0).squeeze(0).numpy()
@@ -16,14 +16,11 @@ def apply_random_eq(X, min=0, max=2):
     eq = (arr1 + arr2 + arr3 + arr4 + arr5 + arr6 + arr7 + arr8 + arr9) / 9.0
     eq = np.clip(eq, min, max)
     eq = np.expand_dims(eq, (0, 2))
-    return X * eq
+    return X * eq, P
 
-def apply_random_volume(X, min=0.5, max=1.5):
-    return X * np.random.uniform(min, max)
-
-def apply_harmonic_distortion(M, P, num_harmonics=2, gain=0.1, n_fft=2048, hop_length=1024):
-    left_M = M[0]
-    right_M = M[1]
+def apply_harmonic_distortion(M, P, c, num_harmonics=2, gain=0.1, n_fft=2048, hop_length=1024):
+    left_M = M[0] / c
+    right_M = M[1] / c
     left_P = P[0]
     right_P = P[1]
     
@@ -44,11 +41,11 @@ def apply_harmonic_distortion(M, P, num_harmonics=2, gain=0.1, n_fft=2048, hop_l
     left_X = librosa.stft(left_ds, n_fft=n_fft, hop_length=hop_length)
     right_X = librosa.stft(right_ds, n_fft=n_fft, hop_length=hop_length)
 
-    return np.array([np.abs(left_X), np.abs(right_X)])
+    return np.array([np.abs(left_X), np.abs(right_X)]) * c, np.array([np.angle(left_X), np.angle(right_X)])
 
-def apply_pitch_shift(M, P, n_fft=2048, hop_length=1024, sr=44100, n_steps=1):
-    left_M = M[0]
-    right_M = M[1]
+def apply_pitch_shift(M, P, c, n_fft=2048, hop_length=1024, sr=44100, n_steps=1):
+    left_M = M[0] / c
+    right_M = M[1] / c
     left_P = P[0]
     right_P = P[1]
     
@@ -64,11 +61,11 @@ def apply_pitch_shift(M, P, n_fft=2048, hop_length=1024, sr=44100, n_steps=1):
     left_X = librosa.stft(left_s, n_fft=n_fft, hop_length=hop_length)
     right_X = librosa.stft(right_s, n_fft=n_fft, hop_length=hop_length)
 
-    return np.array([np.abs(left_X), np.abs(right_X)])
+    return np.array([np.abs(left_X), np.abs(right_X)]) * c, np.array([np.angle(left_X), np.angle(right_X)])
 
-def apply_emphasis(M, P, coef, n_fft=2048, hop_length=1024):
-    left_M = M[0]
-    right_M = M[1]
+def apply_emphasis(M, P, c, emphasis_coef, n_fft=2048, hop_length=1024):
+    left_M = M[0] / c
+    right_M = M[1] / c
     left_P = P[0]
     right_P = P[1]
     
@@ -79,50 +76,47 @@ def apply_emphasis(M, P, coef, n_fft=2048, hop_length=1024):
     right_s = librosa.istft(right_X, hop_length=hop_length)
     
     if np.random.uniform() < 0.5:
-        left_s = librosa.effects.preemphasis(left_s, coef=coef)
-        right_s = librosa.effects.preemphasis(right_s, coef=coef)
+        left_s = librosa.effects.preemphasis(left_s, coef=emphasis_coef)
+        right_s = librosa.effects.preemphasis(right_s, coef=emphasis_coef)
     else:
-        left_s = librosa.effects.deemphasis(left_s, coef=coef)
-        right_s = librosa.effects.deemphasis(right_s, coef=coef)
+        left_s = librosa.effects.deemphasis(left_s, coef=emphasis_coef)
+        right_s = librosa.effects.deemphasis(right_s, coef=emphasis_coef)
 
     left_X = librosa.stft(left_s, n_fft=n_fft, hop_length=hop_length)
     right_X = librosa.stft(right_s, n_fft=n_fft, hop_length=hop_length)
 
-    return np.array([np.abs(left_X), np.abs(right_X)])
+    return np.array([np.abs(left_X), np.abs(right_X)]) * c, np.array([np.angle(left_X), np.angle(right_X)])
 
-def apply_random_phase_noise(P, strength=0.1):
+def apply_random_phase_noise(X, P, strength=0.1):
     random_phase = np.random.uniform(-np.pi, np.pi, size=P.shape)
-    return P + strength * random_phase
+    return X, P + strength * random_phase
 
-def apply_stereo_spatialization(X, c, alpha=1):
-    X = X / c
+def apply_stereo_spatialization(X, P, alpha=1):
     left, right = X[0], X[1]
     avg = (left + right) * 0.5
     left = alpha * left + (1 - alpha) * avg 
     right = alpha * right + (1 - alpha) * avg
-    return np.clip(np.stack([left, right], axis=0), 0, 1) * c
+    return np.clip(np.stack([left, right], axis=0), 0, 1), P
 
-def apply_multiplicative_noise(X, loc=1, scale=0.1):
+def apply_multiplicative_noise(X, P, loc=1, scale=0.1):
     eps = np.random.normal(loc, scale, size=X.shape)
-    return X * eps
+    return X * eps, P
 
-def apply_dynamic_range_mod(X, threshold=0.5, ratio=4):
+def apply_dynamic_range_mod(X, P, c, threshold=0.5, ratio=4):
+    X = X / c
+
     if np.random.uniform() < 0.5:
         clipped = np.clip(X - threshold, 0, None)
         compressed = clipped / ratio
-        return np.where(X > threshold, compressed + threshold, X)
+        return np.where(X > threshold, compressed + threshold, X) * c, P
     else:
         clipped = np.clip(X - threshold, None, 0)
         expanded = clipped * ratio
-        return np.where(X < threshold, expanded + threshold, X)
+        return np.where(X < threshold, expanded + threshold, X) * c, P
     
-def apply_channel_drop(X):
-    if np.random.uniform() < 0.5:
-        X[0] = 0
-    else:
-        X[1] = 0
-
-    return X
+def apply_channel_drop(X, P, channel):
+    X[channel] = 0
+    return X, P
 
 def apply_time_stretch(X, target_size):
     if X.shape[2] > target_size:
@@ -132,7 +126,6 @@ def apply_time_stretch(X, target_size):
         H = X[:, :, :target_size]
         H.real = F.interpolate(torch.from_numpy(cropped.real).unsqueeze(0), size=(X.shape[1], target_size), mode='bilinear', align_corners=True).squeeze(0).numpy()
         H.imag = F.interpolate(torch.from_numpy(cropped.imag).unsqueeze(0), size=(X.shape[1], target_size), mode='bilinear', align_corners=True).squeeze(0).numpy()
-        return H
     else:
         if np.random.uniform() < 0.5:
             padded = np.pad(X, ((0, 0), (0, 0), (np.random.randint(0, target_size // 4), (target_size - X.shape[2]) + np.random.randint(0, target_size // 4))))
@@ -142,7 +135,6 @@ def apply_time_stretch(X, target_size):
             H = padded[:, :, :target_size]
             H.real = F.interpolate(torch.from_numpy(cropped.real).unsqueeze(0), size=(padded.shape[1], target_size), mode='bilinear', align_corners=True).squeeze(0).numpy()
             H.imag = F.interpolate(torch.from_numpy(cropped.imag).unsqueeze(0), size=(padded.shape[1], target_size), mode='bilinear', align_corners=True).squeeze(0).numpy()
-            return H
         else: 
             size = np.random.randint(target_size // 8, target_size)
             start = np.random.randint(0, X.shape[2] - size)
@@ -150,18 +142,19 @@ def apply_time_stretch(X, target_size):
             H = X[:, :, :target_size]
             H.real = F.interpolate(torch.from_numpy(cropped.real).unsqueeze(0), size=(X.shape[1], target_size), mode='bilinear', align_corners=True).squeeze(0).numpy()
             H.imag = F.interpolate(torch.from_numpy(cropped.imag).unsqueeze(0), size=(X.shape[1], target_size), mode='bilinear', align_corners=True).squeeze(0).numpy()
-            return H
+
+    return H
         
-def apply_time_masking(X, max_mask_percentage=0.2):
+def apply_time_masking(X, P, max_mask_percentage=0.2):
     mask_percentage = np.random.uniform(0, max_mask_percentage)
     mask_width = int(X.shape[2] * mask_percentage)
     mask_start = np.random.randint(0, X.shape[2] - mask_width)
     X[:, :, mask_start:mask_start+mask_width] = 0
-    return X
-        
-def apply_frequency_masking(X, max_mask_percentage=0.2):
+    return X, P
+
+def apply_frequency_masking(X, P, max_mask_percentage=0.2):
     mask_percentage = np.random.uniform(0, max_mask_percentage)
     mask_height = int(X.shape[1] * mask_percentage)
     mask_start = np.random.randint(0, X.shape[1] - mask_height)
     X[:, mask_start:mask_start+mask_height, :] = 0
-    return X
+    return X, P
--- libft2gan/dataset_voxaug.py
+++ libft2gan/dataset_voxaug.py
@@ -59,26 +59,25 @@ class VoxAugDataset(torch.utils.data.Dataset):
         M = np.abs(V)
 
         augmentations = [
-            (0.025, apply_channel_drop, {}),
-            (0.5, apply_dynamic_range_mod, { "threshold": np.random.uniform(0, 0.5), "ratio": np.random.randint(2,8) }),
-            (0.2, apply_harmonic_distortion, { "P": P, "num_harmonics": np.random.randint(1, 5), "gain": np.random.uniform(0, 0.5), "n_fft": self.n_fft, "hop_length": self.hop_length }),
-            (0.5, apply_multiplicative_noise, { "loc": 1, "scale": np.random.uniform(0, 0.25) }),
-            (0.5, apply_random_eq, { "min": np.random.uniform(0, 1), "max": np.random.uniform(1, 2) }),
-            (0.5, apply_stereo_spatialization, { "c": Vc, "alpha": np.random.uniform(0, 2) }),
-            (0.2, apply_pitch_shift, { "P": P, "n_fft": self.n_fft, "hop_length": self.hop_length, "sr": self.sr, "n_steps": np.random.uniform(-5, 5) }),
+            (0.1, apply_channel_drop, { "channel": 0}),
+            (0.1, apply_channel_drop, { "channel": 1}),
+            (0.2, apply_dynamic_range_mod, { "c": Vc, "threshold": np.random.uniform(0, 0.5), "ratio": np.random.randint(1,2) }),
+            (0.2, apply_harmonic_distortion, { "c": Vc, "num_harmonics": np.random.randint(1, 5), "gain": np.random.uniform(0, 0.5), "n_fft": self.n_fft, "hop_length": self.hop_length }),
+            (0.2, apply_multiplicative_noise, { "loc": 1, "scale": np.random.uniform(0, 0.25) }),
+            (0.2, apply_random_eq, { "min": np.random.uniform(0, 1), "max": np.random.uniform(1, 2) }),
+            (0.2, apply_stereo_spatialization, { "alpha": np.random.uniform(0, 2) }),
+            (0.2, apply_pitch_shift, { "c": Vc, "n_fft": self.n_fft, "hop_length": self.hop_length, "sr": self.sr, "n_steps": np.random.uniform(-3, 3) }),
             (0.2, apply_time_masking, { "max_mask_percentage": np.random.uniform(0, 0.3) }),
             (0.2, apply_frequency_masking, { "max_mask_percentage": np.random.uniform(0, 0.3) }),
-            (0.2, apply_emphasis, { "P": P, "coef": np.random.uniform(0, 0.3), "n_fft": self.n_fft, "hop_length": self.hop_length })
+            (0.2, apply_emphasis, { "c": Vc, "emphasis_coef": np.random.uniform(0.9, 1), "n_fft": self.n_fft, "hop_length": self.hop_length }),
+            (0.2, apply_random_phase_noise, { "strength": np.random.uniform(0, 0.3)})
         ]
 
         random.shuffle(augmentations)
-        
+
         for p, aug, args in augmentations:
             if np.random.uniform() < p:
-                M = aug(M, **args)
-
-        if np.random.uniform() < 0.5:
-            P = apply_random_phase_noise(P, np.random.uniform(0, 0.3))
+                M, P = aug(M, P, **args)
 
         V = M * np.exp(1.j * P)
 
@@ -96,16 +95,17 @@ class VoxAugDataset(torch.utils.data.Dataset):
         M = np.abs(X)
 
         augmentations = [
-            (0.01, apply_channel_drop, {}),
+            (0.03, apply_channel_drop, { "channel": 0}),
+            (0.03, apply_channel_drop, { "channel": 1}),
             (0.5, apply_random_eq, { "min": np.random.uniform(0.8,1), "max": np.random.uniform(1, 1.2) }),
-            (0.5, apply_stereo_spatialization, { "c": c, "alpha": np.random.uniform(0.8, 1.2) })
+            (0.5, apply_stereo_spatialization, { "alpha": np.random.uniform(0.8, 1.2) })
         ]
 
         random.shuffle(augmentations)
 
         for p, aug, args in augmentations:
             if np.random.uniform() < p:
-                M = aug(M, **args)
+                M, P = aug(M, P, **args)
 
         X = M * np.exp(1.j * P)
 
