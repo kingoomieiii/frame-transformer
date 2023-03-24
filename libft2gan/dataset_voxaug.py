73539c9e3e57528384451ef9fc84d633a7c1282b
carperbr <carperbr@gmail.com>
2023-03-24 12:48:31 -0400

    having trouble with dynamic range mod, disabling

--- libft2gan/dataset_voxaug.py
+++ libft2gan/dataset_voxaug.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 import torch.utils.data
 import torch.nn.functional as F
-from libft2gan.dataset_utils import apply_channel_drop, apply_dynamic_range_mod, apply_harmonic_distortion, apply_multiplicative_noise, apply_random_eq, apply_stereo_spatialization, apply_time_stretch, apply_pitch_shift, apply_random_phase_noise, apply_time_masking, apply_frequency_masking, apply_emphasis
+from libft2.dataset_utils import apply_channel_drop, apply_dynamic_range_mod, apply_harmonic_distortion, apply_multiplicative_noise, apply_random_eq, apply_stereo_spatialization, apply_time_stretch, apply_pitch_shift, apply_random_phase_noise, apply_time_masking, apply_frequency_masking, apply_emphasis
 
 class VoxAugDataset(torch.utils.data.Dataset):
     def __init__(self, path=[], vocal_path=[], is_validation=False, n_fft=2048, hop_length=1024, cropsize=256, sr=44100, seed=0, inst_rate=0.01, data_limit=None, predict_vocals=False, time_scaling=True):
@@ -61,15 +61,14 @@ class VoxAugDataset(torch.utils.data.Dataset):
         augmentations = [
             (0.1, apply_channel_drop, { "channel": 0}),
             (0.1, apply_channel_drop, { "channel": 1}),
-            (0.2, apply_dynamic_range_mod, { "c": Vc, "threshold": np.random.uniform(0, 0.5), "ratio": np.random.randint(1,2) }),
-            (0.2, apply_harmonic_distortion, { "c": Vc, "num_harmonics": np.random.randint(1, 5), "gain": np.random.uniform(0, 0.5), "n_fft": self.n_fft, "hop_length": self.hop_length }),
+            (0.2, apply_harmonic_distortion, { "c": Vc, "num_harmonics": np.random.randint(1, 8), "gain": np.random.uniform(0, 0.5), "n_fft": self.n_fft, "hop_length": self.hop_length }),
             (0.2, apply_multiplicative_noise, { "loc": 1, "scale": np.random.uniform(0, 0.25) }),
-            (0.2, apply_random_eq, { "min": np.random.uniform(0, 1), "max": np.random.uniform(1, 2) }),
-            (0.2, apply_stereo_spatialization, { "alpha": np.random.uniform(0, 2) }),
-            (0.2, apply_pitch_shift, { "c": Vc, "n_fft": self.n_fft, "hop_length": self.hop_length, "sr": self.sr, "n_steps": np.random.uniform(-3, 3) }),
+            (0.2, apply_random_eq, { "min": np.random.uniform(0.5, 1), "max": np.random.uniform(1, 1.5) }),
+            (0.2, apply_stereo_spatialization, { "alpha": np.random.uniform(0.5, 1.5) }),
+            (0.2, apply_pitch_shift, { "c": Vc, "n_fft": self.n_fft, "hop_length": self.hop_length, "sr": self.sr, "n_steps": np.random.uniform(-4, 4) }),
             (0.2, apply_time_masking, { "max_mask_percentage": np.random.uniform(0, 0.3) }),
             (0.2, apply_frequency_masking, { "max_mask_percentage": np.random.uniform(0, 0.3) }),
-            (0.2, apply_emphasis, { "c": Vc, "emphasis_coef": np.random.uniform(0.9, 1), "n_fft": self.n_fft, "hop_length": self.hop_length }),
+            (0.2, apply_emphasis, { "c": Vc, "emphasis_coef": np.random.uniform(0.8, 1), "n_fft": self.n_fft, "hop_length": self.hop_length }),
             (0.2, apply_random_phase_noise, { "strength": np.random.uniform(0, 0.3)})
         ]
 
@@ -95,10 +94,10 @@ class VoxAugDataset(torch.utils.data.Dataset):
         M = np.abs(X)
 
         augmentations = [
-            (0.03, apply_channel_drop, { "channel": 0}),
-            (0.03, apply_channel_drop, { "channel": 1}),
-            (0.5, apply_random_eq, { "min": np.random.uniform(0.8,1), "max": np.random.uniform(1, 1.2) }),
-            (0.5, apply_stereo_spatialization, { "alpha": np.random.uniform(0.8, 1.2) })
+            (0.1, apply_channel_drop, { "channel": 0}),
+            (0.1, apply_channel_drop, { "channel": 1}),
+            (0.2, apply_random_eq, { "min": np.random.uniform(0.8,1), "max": np.random.uniform(1, 1.2) }),
+            (0.2, apply_stereo_spatialization, { "alpha": np.random.uniform(0.8, 1.2) })
         ]
 
         random.shuffle(augmentations)
