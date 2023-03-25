09a002d003ce1cd7829a7e031f27c9cb2ec953bf
carperbr <carperbr@gmail.com>
2023-03-25 00:19:15 -0400

    moving custom schedulers to libft folders

--- libft2/lr_scheduler_linear_warmup.py
+++ libft2/lr_scheduler_linear_warmup.py
@@ -29,3 +29,19 @@ class LinearWarmupScheduler(_LRScheduler):
 
     def load_state_dict(self, state_dict):
         self.__dict__.update(state_dict)
+
+class LinearWarmup:
+    def __init__(self, target_lr=1e-3, num_steps=16000, current_step=0, verbose_skip_steps=1000):
+        self.num_steps = num_steps
+        self.starting_step = current_step
+        self.current_step = current_step
+        self.verbose_skip_steps = verbose_skip_steps
+
+        self.target_lr = target_lr
+        self.current_lr = 0
+
+    def step(self):
+        if self.current_step < self.num_steps+1:
+            self.current_lr = (self.current_step+1) * (self.target_lr / (self.num_steps+1))
+
+        self.current_step = self.current_step + 1
