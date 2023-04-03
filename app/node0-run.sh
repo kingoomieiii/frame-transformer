3c833120d86d18dbdcf1aa1cb9f5ac2dced70649
carperbr <carperbr@gmail.com>
2023-04-03 03:48:15 -0400

    adding other nodes scripts

---
+++ app/node0-run.sh
@@ -0,0 +1,13 @@
+export MASTER_ADDR="10.0.0.34"
+export MASTER_PORT=1234
+
+python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=2 --node_rank=0 --master_addr="${MASTER_ADDR}" --master_port="${MASTER_PORT}" \
+            train-v1cgan-pre.py \
+                --gpu 0 \
+                --instrumental_lib '/root/data/instruments_p1|/root/data/instruments_p2' \
+                --pretraining_lib '/root/data/pretraining_p1' \
+                --vocal_lib '/root/data/vocals_p1|/root/data/vocals_p2' \
+                --validation_lib '/root/data/validation' \
+                --model_dir '/root/data/models' \
+                --world_rank 0 \
+                --distributed true
--- app/node1-run.sh
+++ app/node1-run.sh
@@ -1,7 +1,7 @@
 export MASTER_ADDR="10.0.0.34"
 export MASTER_PORT=1234
 
-python3 -m torch.distributed.launch --nproc_per_node=8 --nnodes=2 --node_rank=1 --master_addr="${MASTER_ADDR}" --master_port="${MASTER_PORT}" \
+python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=2 --node_rank=1 --master_addr="${MASTER_ADDR}" --master_port="${MASTER_PORT}" \
             train-v1cgan-pre.py \
                 --gpu 0 \
                 --instrumental_lib '/root/data/instruments_p1|/root/data/instruments_p2|/root/data/instruments_p3|/root/data/instruments_p4|/root/data/instruments_p5' \
