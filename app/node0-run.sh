a14ba777e9129768fa0cd1a388f873e1affbd549
carperbr <carperbr@gmail.com>
2023-04-03 03:59:53 -0400

    updating run scripts

--- app/node0-run.sh
+++ app/node0-run.sh
@@ -4,6 +4,9 @@ export MASTER_PORT=1234
 python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=2 --node_rank=0 --master_addr="${MASTER_ADDR}" --master_port="${MASTER_PORT}" \
             train-v1cgan-pre.py \
                 --gpu 0 \
+                --batch_sizes 8 \
+                --cropsizes 256 \
+                --accumulation_steps 1 \
                 --instrumental_lib '/root/data/instruments_p1|/root/data/instruments_p2' \
                 --pretraining_lib '/root/data/pretraining_p1' \
                 --vocal_lib '/root/data/vocals_p1|/root/data/vocals_p2' \
--- app/node1-run.sh
+++ app/node1-run.sh
@@ -4,6 +4,9 @@ export MASTER_PORT=1234
 python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=2 --node_rank=1 --master_addr="${MASTER_ADDR}" --master_port="${MASTER_PORT}" \
             train-v1cgan-pre.py \
                 --gpu 0 \
+                --batch_sizes 8 \
+                --cropsizes 256 \
+                --accumulation_steps 1 \
                 --instrumental_lib '/root/data/instruments_p1|/root/data/instruments_p2|/root/data/instruments_p3|/root/data/instruments_p4|/root/data/instruments_p5' \
                 --pretraining_lib '/root/data/pretraining_p1|/root/data/pretraining_p2|/root/data/pretraining_p3|/root/data/pretraining_p4|/root/data/pretraining_p5' \
                 --vocal_lib '/root/data/vocals_p1|/root/data/vocals_p2' \
