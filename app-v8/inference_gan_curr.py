9676043d4656d0ff9e5700eebc17794c15dd1269
kingoomieiii <kingoomieiii@gmail.com>
2023-10-21 20:59:16 -0400

    Squashed commit of the following:
    commit 09ea46032ed721388c8cfc4aed8caccfb15f36d0
    Author: carperbr <carperbr@gmail.com>
    Date:   Fri Oct 20 18:12:40 2023 -0400
    
        updating checkpoint to a newer version
    
    commit 692a8a7c24d35e063f5ed8514a3b56441fb7a887
    Author: carperbr <carperbr@gmail.com>
    Date:   Fri Oct 20 18:11:06 2023 -0400
    
        updates for saving metadata and various other things
    
    commit 81dbbd738afd37c1894e410deac79919302dd357
    Merge: ec1a25c 031f3d8
    Author: Benjamin Carper <30326384+carperbr@users.noreply.github.com>
    Date:   Fri Oct 20 18:02:01 2023 -0400
    
        Merge pull request #8 from kingoomieiii/master
    
        Convenience updates + format change
    
    commit ec1a25cdf35f99e1729f81c2edd5bd0d2c7e09d8
    Author: carperbr <carperbr@gmail.com>
    Date:   Thu Oct 19 13:07:17 2023 -0400
    
        testing out new architecture

--- app-v8/inference_gan_curr.py
+++ app-v8/inference_gan_curr.py
@@ -4,15 +4,25 @@ import shutil
 import librosa
 import numpy as np
 import soundfile as sf
-import music_tag
 import torch
+import json
+import time
+
+import shlex, subprocess
+
 from tqdm import tqdm
-from libft2gan.frame_transformer4 import FrameTransformerGenerator
+from libft2gancurr.frame_transformer2 import FrameTransformerGenerator
+
+from torch.nn import functional as F
+
 from lib import dataset
 from lib import spec_utils
 from lib import utils
+from lib import nets
+
 
 class Separator(object):
+
     def __init__(self, corrector, model, device, batchsize, cropsize, n_fft, postprocess=False):
         self.corrector = corrector
         self.model = model
@@ -34,7 +44,7 @@ class Separator(object):
             X_mag_crop = X_mag_pad[:, :, (start - padding):(start + cropsize + padding)]
             X_dataset.append(X_mag_crop)
 
-        self.model.eval()
+        self.model.train()
         with torch.no_grad():
             mask = []
             # To reduce the overhead, dataloader is not used.
@@ -42,7 +52,7 @@ class Separator(object):
                 X_batch = X_dataset[i: i + self.batchsize]
                 X_batch = torch.from_numpy(np.asarray(X_batch)).to(self.device)[:, :, :(self.n_fft // 2)]
 
-                with torch.cuda.amp.autocast_mode.autocast(enabled=True):
+                with torch.cuda.amp.autocast_mode.autocast():
                     pred = torch.sigmoid(self.model(X_batch))
 
                 if padding > 0:
@@ -76,10 +86,12 @@ class Separator(object):
 
     def separate(self, X_spec, padding=None):
         X_mag, X_phase = self._preprocess(X_spec)
+
         n_frame = X_mag.shape[2]
         pad_l, pad_r, _ = dataset.make_padding(n_frame, self.cropsize, 0)
-        xm = X_mag / X_mag.max()
-        X_mag_pad = np.pad(xm, ((0, 0), (0, 0), (pad_l, pad_r)), mode='constant')
+        X_mag_pad = np.pad(X_mag, ((0, 0), (0, 0), (pad_l, pad_r)), mode='constant')
+        X_mag_pad /= X_mag_pad.max()
+
         mask = self._separate(X_mag_pad, self.cropsize, padding)
 
         mask = mask[:, :, :n_frame]
@@ -109,23 +121,20 @@ class Separator(object):
 def main():
     p = argparse.ArgumentParser()
     p.add_argument('--gpu', '-g', type=int, default=-1)
-    p.add_argument('--pretrained_model', '-P', type=str, default='model.pth') # https://mega.nz/file/L55UwZAC#leWuP1ic9Rt4SpDVHeo6tnimaEIw5095ORoiaosF1Do
+    p.add_argument('--pretrained_model', '-P', type=str, default='/media/ben/internal-nvme-b/models/local.52.modelt.pth')
     p.add_argument('--input', '-i', required=True)
     p.add_argument('--output', '-o', type=str, default="")
-    p.add_argument('--output_format', type=str, default="flac")
     p.add_argument('--num_res_encoders', type=int, default=4)
     p.add_argument('--num_res_decoders', type=int, default=4)
     p.add_argument('--sr', '-r', type=int, default=44100)
     p.add_argument('--n_fft', '-f', type=int, default=2048)
     p.add_argument('--hop_length', '-H', type=int, default=1024)
-    p.add_argument('--batchsize', '-B', type=int, default=4)
+    p.add_argument('--batchsize', '-B', type=int, default=1)
     p.add_argument('--cropsize', '-c', type=int, default=1024)
     p.add_argument('--padding', type=int, default=512)
     p.add_argument('--output_image', '-I', action='store_true')
-    p.add_argument('--copy_source_images', action='store_true') # copies images from input into output
     p.add_argument('--postprocess', '-p', action='store_true')
     p.add_argument('--create_webm', action='store_true')
-    p.add_argument('--create_vocals', action='store_true')
     p.add_argument('--num_encoders', type=int, default=2)
     p.add_argument('--num_decoders', type=int, default=13)
     p.add_argument('--tta', '-t', action='store_true')
@@ -133,16 +142,11 @@ def main():
     p.add_argument('--depth', type=int, default=7)
     p.add_argument('--num_transformer_blocks', type=int, default=2)
     p.add_argument('--bias', type=str, default='true')
-    p.add_argument('--rename_dir', type=str, default='false')
-    
-    p.add_argument('--num_attention_maps', type=int, default=2)
+
     p.add_argument('--channels', type=int, default=8)
-    p.add_argument('--num_bridge_layers', type=int, default=4)
-    p.add_argument('--latent_expansion', type=int, default=4)
-    p.add_argument('--expansion', type=int, default=4)
+    p.add_argument('--expansion', type=int, default=10240)
     p.add_argument('--num_heads', type=int, default=8)
-    p.add_argument('--dropout', type=float, default=0.35)
-    p.add_argument('--weight_decay', type=float, default=1e-2)
+    p.add_argument('--dropout', type=float, default=0.1)
 
     p.add_argument('--seed', type=int, default=1)
 
@@ -150,8 +154,8 @@ def main():
     p.add_argument('--feedforward_expansion', type=int, default=24)
 
     args = p.parse_args()
+
     args.cropsizes = [int(cropsize) for cropsize in args.cropsizes.split(',')]
-    args.rename_dir = str.lower(args.rename_dir) == 'true'
 
     print('loading model...', end=' ')
     device = torch.device('cpu')
@@ -159,7 +163,7 @@ def main():
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
 
-    model = FrameTransformerGenerator(in_channels=2, out_channels=2, channels=args.channels, expansion=args.expansion, n_fft=args.n_fft, dropout=args.dropout, num_heads=args.num_heads, num_attention_maps=args.num_attention_maps, num_bridge_layers=args.num_bridge_layers, latent_expansion=args.latent_expansion)
+    model = FrameTransformerGenerator(in_channels=2, channels=args.channels, n_fft=args.n_fft, expansion=args.expansion, num_heads=args.num_heads)
     model.load_state_dict(torch.load(args.pretrained_model, map_location=device))
 
     if torch.cuda.is_available() and args.gpu >= 0:
@@ -168,22 +172,22 @@ def main():
     print('done')
 
     output_folder = args.output
-    if output_folder != '' and not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-            
-    output_format = args.output_format.lower()
-    if output_format not in ["flac", "wav", "mp3"]:
-        output_format = "flac"
+
+    process = None
+    queue = []
 
     if os.path.isdir(args.input):
         args.input = os.path.join(args.input, '')
         d = os.path.basename(os.path.dirname(args.input))
         output_folder = os.path.join(output_folder, d)
         print(output_folder)
-        extensions = ['wav', 'm4a', 'mp3', 'mp4', 'flac', 'ogg']
+        extensions = ['wav', 'm4a', 'mp3', 'mp4', 'flac']
         cover_ext = ["jpg", "png", "bmp"]
 
         cover = ""
+
+        if output_folder != '' and not os.path.exists(output_folder):
+            os.makedirs(output_folder)
 
         files = []
         d = os.listdir(args.input)
@@ -195,8 +199,6 @@ def main():
 
             if ext in cover_ext:
                 cover = os.path.join(args.input, f)
-                if args.copy_source_images:
-                    shutil.copy(cover, output_folder)	
 
         for file in tqdm(files):
             print('loading wave source...', end=' ')
@@ -222,40 +224,32 @@ def main():
             print('inverse stft of instruments...', end=' ')
             wave = spec_utils.spectrogram_to_wave(y_spec, hop_length=args.hop_length)
             print('done')
-            inst_file = f'{output_folder}/{basename}_Instruments.{output_format}'
+            inst_file = '{}/{}_Instruments.wav'.format(output_folder, basename)
             sf.write(inst_file, wave.T, sr)
+            vid_file = '{}/{}.mp4'.format(output_folder, basename)
 
-            filetags = music_tag.load_file(file)
-            inst_tags = music_tag.load_file(inst_file)
-            inst_tags['tracktitle'] = filetags['tracktitle']
-            inst_tags['album'] = filetags['album']
-            inst_tags['artist'] = filetags['artist']
-            inst_tags['tracknumber'] = filetags['tracknumber']
-            inst_tags['totaltracks'] = filetags['totaltracks']
-            inst_tags['year'] = filetags['year']
-            inst_tags.save()
 
             if args.create_webm:
-                vid_file = f'{output_folder}/{basename}.mp4'
                 os.system(f'ffmpeg -y -framerate 1 -loop 1 -i "{cover}" -i "{inst_file}" -t {librosa.get_duration(wave, sr=args.sr)} "{vid_file}"')
 
-            if args.create_vocals:
-                print('inverse stft of vocals...', end=' ')
-                wave = spec_utils.spectrogram_to_wave(v_spec, hop_length=args.hop_length)
-                print('done')
-                sf.write(f'{output_folder}/{basename}_Vocals.{output_format}', wave.T, sr)
+            print('inverse stft of vocals...', end=' ')
 
-                vocal_tags = music_tag.load_file(f'{output_folder}/{basename}_Vocals.{output_format}')
-                vocal_tags['tracktitle'] = f'{filetags["tracktitle"]} (Vocals)'
-                vocal_tags['album'] = f'{filetags["album"]} (Vocals)'
-                vocal_tags['artist'] = filetags['artist']
-                vocal_tags['tracknumber'] = filetags['tracknumber']
-                vocal_tags['totaltracks'] = filetags['totaltracks']
-                vocal_tags['year'] = filetags['year']
-                vocal_tags.save()
+            wave = spec_utils.spectrogram_to_wave(v_spec, hop_length=args.hop_length)
+            print('done')
+            sf.write('{}/{}_Vocals.wav'.format(output_folder, basename), wave.T, sr)
 
-        if args.rename_dir:
-            os.system(f'python song-renamer.py --dir "{output_folder}"')
+            if args.output_image:
+                try:
+                    image = spec_utils.spectrogram_to_image(y_spec)
+                    utils.imwrite('{}{}_Instruments.jpg'.format(output_folder, basename), image)
+                    image = spec_utils.spectrogram_to_image(v_spec)
+                    utils.imwrite('{}{}_Vocals.jpg'.format(output_folder, basename), image)
+                    image = np.uint8(m_spec)
+                    image = np.pad(image, ((0,1), (0,0), (0,0)))
+                    image = image.transpose(1, 2, 0)
+                    utils.imwrite('{}{}_Mask.jpg'.format(output_folder, basename), image)
+                except:
+                    pass
             
     else:
         print('loading wave source...', end=' ')
@@ -272,7 +266,7 @@ def main():
         X_spec = spec_utils.wave_to_spectrogram(X, args.hop_length, args.n_fft)
         print('done')
 
-        sp = Separator(None, model, device, args.batchsize, args.cropsize, args.n_fft, args.postprocess)
+        sp = Separator(model, device, args.batchsize, args.cropsize, args.postprocess)
 
         if args.tta:
             y_spec, v_spec, m_spec = sp.separate_tta(X_spec)
@@ -282,32 +276,15 @@ def main():
         print('inverse stft of instruments...', end=' ')
         wave = spec_utils.spectrogram_to_wave(y_spec, hop_length=args.hop_length)
         print('done')
-        sf.write(f'{output_folder}/{basename}_Instruments.{output_format}', wave.T, sr)
+        sf.write('{}_Instruments.wav'.format(basename), wave.T, sr)
 
-        filetags = music_tag.load_file(file)
-        inst_tags = music_tag.load_file(f'{output_folder}\{basename}_Instruments.{args.output_format}')
-        inst_tags['tracktitle'] = f'{filetags["tracktitle"]} (Instruments)'
-        inst_tags['album'] = f'{filetags["album"]} (Instruments)'
-        inst_tags['artist'] = filetags['artist']
-        inst_tags['tracknumber'] = filetags['tracknumber']
-        inst_tags['totaltracks'] = filetags['totaltracks']
-        inst_tags['year'] = filetags['year']
-        inst_tags.save()
-
-        if args.create_vocals:
-            print('inverse stft of vocals...', end=' ')
-            wave = spec_utils.spectrogram_to_wave(v_spec, hop_length=args.hop_length)
-            print('done')
-            sf.write(f'{output_folder}/{basename}_Vocals.{output_format}', wave.T, sr)
-            
-            vocal_tags = music_tag.load_file(f'{output_folder}\{basename}_Vocals.{args.output_extension}')
-            vocal_tags['tracktitle'] = f'{filetags["tracktitle"]} (Vocals)'
-            vocal_tags['album'] = f'{filetags["album"]} (Vocals)'
-            vocal_tags['artist'] = filetags['artist']
-            vocal_tags['tracknumber'] = filetags['tracknumber']
-            vocal_tags['totaltracks'] = filetags['totaltracks']
-            vocal_tags['year'] = filetags['year']
-            vocal_tags.save()
+        print('inverse stft of vocals...', end=' ')
+        c = np.abs(y_spec).max()
+        v_spec = c.copy()
+        v_spec.real = (v_spec.real / c + np.random.normal(size=v_spec.shape)) * c
+        wave = spec_utils.spectrogram_to_wave(v_spec, hop_length=args.hop_length)
+        print('done')
+        sf.write('{}_Vocals.wav'.format(basename), wave.T, sr)
 
         if args.output_image:
             image = spec_utils.spectrogram_to_image(y_spec)
